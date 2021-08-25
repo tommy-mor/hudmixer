c539148def811e7a96fc732fd2336e58dcf29ea6
tommy-mor <thmorriss@gmail.com>
2021-08-24 17:20:50 -0700

    added basic interface

--- src/ui/interface.py
+++ src/ui/interface.py
@@ -1,9 +1,12 @@
 import sys
 
-from qtpy.QtWidgets import QMainWindow, QTreeWidget, QTreeWidgetItem, QApplication
+from qtpy.QtWidgets import QMainWindow, QTreeWidget, QTreeWidgetItem, QApplication, QFileDialog, QErrorMessage
 from qtpy import QtCore
 
-import src.feature_list as fl
+import model.feature_list as fl
+import model.hud as hud
+
+import traceback
 
 
 class MainWindow(QMainWindow):
@@ -12,8 +15,30 @@ class MainWindow(QMainWindow):
         treeWidget = QTreeWidget(self)
         treeWidget.setColumnCount(2)
 
-        items = [QTreeWidgetItem(cls.__name__) for cls in fl.classes]
-        treeWidget.insertTopLevelItems(0, items)
+        self.nameToClass = {cls.__name__: cls for cls in fl.classes}
+
+        self.items = [QTreeWidgetItem(treeWidget, [name]) for name in self.nameToClass.keys()]
+
+        treeWidget.resizeColumnToContents(0)
+
+        treeWidget.itemClicked['QTreeWidgetItem*', 'int'].connect(self.tree_item_clicked)
         self.setCentralWidget(treeWidget)
 
+    def tree_item_clicked(self, item, n):
+        d = QFileDialog.getExistingDirectory(self, "Open Directory", None, QFileDialog.ShowDirsOnly)
 
+        print(d)
+
+        if d:
+            try:
+                print('trying to import hud %s' % d)
+                newhud = hud.ImportHud(d)
+                print(newhud)
+                item.setData(1, 0, newhud.translation_key)
+                item.setData(100, 0, newhud)
+            except hud.HudException as e:
+                print(e)
+                QErrorMessage(self).showMessage('error importing hud, %s' % e)
+            except AssertionError as e:
+                print('failed loading %s' % d)
+                QErrorMessage(self).showMessage('failed loading %s, %s'% (d, '\n'.join(traceback.format_exception(AssertionError, e, e.__traceback__))))
